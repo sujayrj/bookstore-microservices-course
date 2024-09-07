125103dbe6db1dfb3e693caca4fb75aa62056881
Sujay Jeppu <sujayrj@gmail.com>
2024-09-07 14:57:25 +0530

    catalog-service walking skelton

---
+++ catalog-service/src/test/java/dev/jeppu/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package dev.jeppu.bookstore.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import({ContainersConfig.class})
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        /*
+        test method
+         */
+    }
+
+}
---
+++ catalog-service/src/test/java/dev/jeppu/bookstore/catalog/TestCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.jeppu.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Import;
+
+@Import({ContainersConfig.class})
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main).with(TestCatalogServiceApplication.class).run(args);
+    }
+
+}
